03d9cd6f9f0af5db8d1c8207c5ad6dc4b97ad507
Paul Suda <paulsuda@gmail.com>
2016-11-12 17:22:25 -0600

    Decoding hex input ok now.

--- challenge1/RunChallenge.hs
+++ challenge1/RunChallenge.hs
@@ -1,16 +1,33 @@
 
 
+hexVal :: Char -> Int
+hexVal '0' = 0
+hexVal '1' = 1
+hexVal '2' = 2
+hexVal '3' = 3
+hexVal '4' = 4
+hexVal '5' = 5
+hexVal '6' = 6
+hexVal '7' = 7
+hexVal '8' = 8
+hexVal '9' = 9
+hexVal 'a' = 10
+hexVal 'b' = 11
+hexVal 'c' = 12
+hexVal 'd' = 13
+hexVal 'e' = 14
+hexVal 'f' = 15
 
-decodeBase :: String -> String
-decodeBase "" = ""
-decodeBase (a:b:c:x) = "[" ++ [a] ++ "," ++ [b] ++ "," ++ [c] ++ "]-" ++ decodeBase(x)
+decodeHex :: String -> String
+decodeHex "" = ""
+decodeHex (a:b:x) = [toEnum(hexVal(a) * 16 + hexVal(b))] ++ decodeHex(x)
 
 
 
 main = do
-  inp <- readFile "input.txt"
-  let inpok = init inp
-  let l = show(length(inpok))
-  putStrLn("Input (length " ++ l ++ ")")
-  putStrLn "Result:"
-  putStrLn(decodeBase(inpok))
+  fileInput <- readFile "input.txt"
+  let parseInput = init fileInput
+  let l = show(length(parseInput))
+  let decodedInput = decodeHex(parseInput)
+  putStrLn("Input (length " ++ l ++ "): " ++ parseInput)
+  putStrLn("Decoded Input: " ++ decodedInput)
