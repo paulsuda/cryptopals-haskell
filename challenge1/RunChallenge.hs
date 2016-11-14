06be2d8c458571fff25824df9f3192afb6a04622
Paul Suda <paulsuda@gmail.com>
2016-11-14 14:58:48 -0600

    OK! Challenge 1 done! Totally works. Maybe messy.

--- challenge1/RunChallenge.hs
+++ challenge1/RunChallenge.hs
@@ -1,5 +1,4 @@
 
-
 hexVal :: Char -> Int
 hexVal '0' = 0
 hexVal '1' = 1
@@ -22,12 +21,36 @@ decodeHex :: String -> String
 decodeHex "" = ""
 decodeHex (a:b:x) = [toEnum(hexVal(a) * 16 + hexVal(b))] ++ decodeHex(x)
 
+base64Codes :: [Char]
+base64Codes = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
 
+encodeBase64Char :: Int -> Char
+encodeBase64Char (bits) = base64Codes !! (bits `mod` 64)
+
+encodeBase64 :: String -> String
+encodeBase64 "" = ""
+encodeBase64 (a:b:c:x) = do
+  let bytes = fromEnum(c) + (fromEnum(b) * (2 ^ 8)) + (fromEnum(a) * (2 ^ 16))
+  let zz = putStrLn("x = " ++ x)
+  [ encodeBase64Char(bytes `quot` (64 ^ 3)),  encodeBase64Char(bytes `quot` (64 ^ 2)), encodeBase64Char(bytes `quot` 64), encodeBase64Char(bytes) ] ++ encodeBase64(x)
+
+
+-- readCleanedUpFile :: String -> String
+-- readCleanedUpFile (fileName) = do
+--   fileContents <- readFile fileName
+--   concat(lines(fileContents))
 
 main = do
   fileInput <- readFile "input.txt"
-  let parseInput = init fileInput
-  let l = show(length(parseInput))
-  let decodedInput = decodeHex(parseInput)
-  putStrLn("Input (length " ++ l ++ "): " ++ parseInput)
-  putStrLn("Decoded Input: " ++ decodedInput)
+  let cleanedInput = concat $ lines $ fileInput
+  fileExpectedOutput <- readFile "expected_output.txt"
+  let cleanedExpectedOutput = concat $ lines $ fileExpectedOutput
+  let inputLength = show(length(cleanedInput))
+  let decodedInput = decodeHex(cleanedInput)
+  let base64Output = encodeBase64(decodedInput)
+  let decodedLength = show(length(decodedInput))
+  putStrLn("Input (length " ++ inputLength ++ "): " ++ cleanedInput)
+  putStrLn("Decoded (length " ++ decodedLength ++ "): " ++ decodedInput)
+  putStrLn("Base64: " ++ base64Output)
+  putStrLn("Expected: " ++ cleanedExpectedOutput)
+  putStrLn("Got expected result? " ++ (if base64Output == cleanedExpectedOutput then "YES" else "NO"))
