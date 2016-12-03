20bd2e256905e165ab9fe46adfed418888fcb1c4
Paul Suda <paulsuda@gmail.com>
2016-12-03 12:07:29 -0600

    Challenge 4 works but no result.

--- challenge4/RunChallenge.hs
+++ challenge4/RunChallenge.hs
@@ -4,13 +4,13 @@ module Challenge4.RunChallenge (main, run) where
 import qualified Shared.Hex as Hex
 import Shared.Histogram (HistValue, HistArray, histMax)
 import Shared.KeyScoring (singleByteXorKeyScores)
+import Shared.XorUtils (xorStringChar)
 
 main :: IO ()
 main = run(putStrLn, putStrLn, putStrLn)
 
 bestXorScore :: String -> (Int, HistValue)
-bestXorScore ciphertext = histMax scoreHist
-  where scoreHist = singleByteXorKeyScores ciphertext
+bestXorScore ciphertext = histMax $ singleByteXorKeyScores ciphertext
 
 run (putResult, putError, putStatus) = do
   cipherTextFile <- readFile "challenge4/ciphertext.txt"
@@ -21,7 +21,14 @@ run (putResult, putError, putStatus) = do
   let decryptedString = "foo"
   putStatus("Ciphertext Entries List: (length " ++ show(length cipherTextHexList) ++ ")")
   let samplesBestXorScores = map bestXorScore cipherTextList
-
+  let sampleBestScores = map snd samplesBestXorScores
+  let sampleBestKeys = map fst samplesBestXorScores
+  let (bestSampleIndex, bestSampleScore) = histMax sampleBestScores
+  let bestXorKey = sampleBestKeys !! bestSampleIndex
+  let bestCipherText = cipherTextList !! bestSampleIndex
+  let decryptedString = xorStringChar bestCipherText bestXorKey
+  putStatus("Best sample index: " ++ show bestSampleIndex ++ " with score: " ++ show bestSampleScore)
+  putStatus("Best XOR Key: " ++ show bestXorKey ++ " decrypts to: " ++ decryptedString)
   putStatus("Expected Output (length " ++ show(length expectedOutput) ++ "): " ++ expectedOutput)
   if decryptedString == expectedOutput then putResult "OK! Expected result."
   else putError "ERROR! Result not as expected."
---
+++ shared/KeyScoring.hs
@@ -0,0 +1,21 @@
+module Shared.KeyScoring (singleByteXorKeyScores) where
+
+import Shared.Histogram (HistValue, HistArray)
+import Shared.TextUtils (scoreEnglish)
+import Shared.XorUtils (xorStringChar)
+
+-- Given ciphertext and a test key returns a score how likely it is english text
+scoreKey :: String -> Int -> HistValue
+scoreKey cipherText xorKey = scoreEnglish possiblePlaintext
+  where possiblePlaintext = xorStringChar cipherText xorKey
+
+-- Given an array of keys to test and a cipher text argument, returns
+-- a list of scores for each key.
+testKeyScores :: [Int] -> String -> HistArray
+testKeyScores testKeys cipherText = map (scoreKey cipherText) testKeys
+
+-- Pass in a ciphertext and get a histogram of each of the 0 .. 255 possible
+-- single byte xor keys related to it's likelyhood that it is english text.
+singleByteXorKeyScores :: String -> HistArray
+singleByteXorKeyScores = testKeyScores testKeys
+  where testKeys = [0 .. 255]
