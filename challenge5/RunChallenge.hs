faf0904d3698d42c0b77b0352eac012f0bd0a533
Paul Suda <paulsuda@gmail.com>
2016-12-06 21:52:34 -0600

    Sharing whitespace trim function.

--- challenge5/RunChallenge.hs
+++ challenge5/RunChallenge.hs
@@ -2,28 +2,10 @@
 module Challenge5.RunChallenge (run, main) where
 import qualified Shared.Hex as Hex
 import Shared.XorUtils (xorStrings)
-import Shared.TextUtils (repeatToLength)
+import Shared.TextUtils (repeatToLength, trimWhitespace)
 
 main :: IO ()
 main = run(putStrLn, putStrLn, putStrLn)
-
-isWhitespace :: Char -> Bool
-isWhitespace c = c == ' ' || c == '\n' || c == '\t'
-
-trimWhitespaceStart :: String -> String
-trimWhitespaceStart "" = ""
-trimWhitespaceStart str = if isWhitespace start then nextStr else str
-  where start = head str
-        nextStr = trimWhitespaceStart $ tail str
-
-trimWhitespaceEnd :: String -> String
-trimWhitespaceEnd "" = ""
-trimWhitespaceEnd str = if isWhitespace end then nextStr else str
-  where end = last str
-        nextStr = trimWhitespaceEnd $ init str
-
-trimWhitespace :: String -> String
-trimWhitespace str = (trimWhitespaceStart . trimWhitespaceEnd) str
 
 run (putResult, putError, putStatus) = do
   plainTextFile <- readFile "challenge5/plaintext.txt"
@@ -33,19 +15,12 @@ run (putResult, putError, putStatus) = do
   xorKeyFile <- readFile "challenge5/xor_key.txt"
   let xorKey = concat $ lines xorKeyFile
   let xorKeyRepeated = repeatToLength (length plainText) xorKey
-
   let cipherText = xorStrings plainText xorKeyRepeated
   let encryptedString = Hex.encode cipherText
-
   putStatus("Plaintext: (length " ++ show(length plainText) ++ ") '" ++ plainText ++ "'")
   putStatus("Expected Output: (length " ++ show(length expectedOutput) ++ ") '" ++ expectedOutput ++ "'")
   putStatus("XOR Key: (length " ++ show(length xorKey) ++ ") '" ++ xorKey ++ "'")
   putStatus("Key repeated: '" ++ xorKeyRepeated ++ "'")
   putStatus("Encrypted String: (length " ++ show(length encryptedString) ++ ") '" ++ encryptedString ++ "'")
-
-  -- putStatus("Ciphertext Entries List: (length " ++ show(length cipherTextHexList) ++ ")")
-  -- putStatus("Best sample index: " ++ show bestSampleIndex ++ " with score: " ++ show bestSampleScore)
-  -- putStatus("Best XOR Key: " ++ show bestXorKey ++ " decrypts to: '" ++ decryptedString ++ "'")
-  -- putStatus("Expected Output (length " ++ show(length expectedOutput) ++ "): '" ++ expectedOutput ++ "'")
   if encryptedString == expectedOutput then putResult "OK! Expected result."
   else putError "ERROR! Result not as expected."
--- shared/TextUtils.hs
+++ shared/TextUtils.hs
@@ -1,4 +1,4 @@
-module Shared.TextUtils (scoreEnglish, repeatToLength, subString) where
+module Shared.TextUtils (scoreEnglish, repeatToLength, subString, trimWhitespace) where
 import Shared.Histogram (HistValue, charHistogram)
 
 subString :: String -> Int -> Int -> String
@@ -7,24 +7,24 @@ subString str 0 end = head str : subString (tail str) 0 (pred end)
 subString str start end = subString (tail str) (pred start) (pred end)
 
 repeatToLength :: Int -> String -> String
-repeatToLength totalLength repeatString = (concat $ replicate replicateCount repeatString) ++ subString repeatString 0 padLength
+repeatToLength totalLength repeatString = concat(replicate replicateCount repeatString) ++ subString repeatString 0 padLength
   where replicateCount = quot totalLength repeatLength
         padLength = rem totalLength repeatLength
         repeatLength = length repeatString
-
-wordOccurrenceCount :: String -> String -> Int
-wordOccurrenceCount "" _ = 0
-wordOccurrenceCount testText wordString = if isMatch then returnMatch else returnNoMatch
-  where wordLength = length wordString
-        testTextLength = length testText
-        wordCompareString = wordString
-        testTextCompareString = map (testText !!) [0 .. wordLength - 1]
-        isMatch = wordCompareString == testTextCompareString
-        -- TODO maybe there's a case insensitie cmp
-        testTextAfterFind = map (testText !!) [3 .. testTextLength - 1]
-        textTextAfterChar = map (testText !!) [1 .. testTextLength - 1]
-        returnMatch = 1 + wordOccurrenceCount testTextAfterFind wordString
-        returnNoMatch = wordOccurrenceCount textTextAfterChar wordString
+--
+-- wordOccurrenceCount :: String -> String -> Int
+-- wordOccurrenceCount "" _ = 0
+-- wordOccurrenceCount testText wordString = if isMatch then returnMatch else returnNoMatch
+--   where wordLength = length wordString
+--         testTextLength = length testText
+--         wordCompareString = wordString
+--         testTextCompareString = map (testText !!) [0 .. wordLength - 1]
+--         isMatch = wordCompareString == testTextCompareString
+--         -- TODO maybe there's a case insensitie cmp
+--         testTextAfterFind = map (testText !!) [3 .. testTextLength - 1]
+--         textTextAfterChar = map (testText !!) [1 .. testTextLength - 1]
+--         returnMatch = 1 + wordOccurrenceCount testTextAfterFind wordString
+--         returnNoMatch = wordOccurrenceCount textTextAfterChar wordString
 
 
 -- Given some text, returns a score for likelyhood it is english text.
@@ -42,3 +42,22 @@ scoreEnglish testText = letterE + whiteSpace + (15.0 * wordTheFreq) + (15.0 * wo
 
 
 -- Other tests: contains the word "the" , contains only printable chars, ratio of letters to non letters
+
+
+isWhitespace :: Char -> Bool
+isWhitespace c = c == ' ' || c == '\n' || c == '\t'
+
+trimWhitespaceStart :: String -> String
+trimWhitespaceStart "" = ""
+trimWhitespaceStart str = if isWhitespace start then nextStr else str
+  where start = head str
+        nextStr = trimWhitespaceStart $ tail str
+
+trimWhitespaceEnd :: String -> String
+trimWhitespaceEnd "" = ""
+trimWhitespaceEnd str = if isWhitespace end then nextStr else str
+  where end = last str
+        nextStr = trimWhitespaceEnd $ init str
+
+trimWhitespace :: String -> String
+trimWhitespace = trimWhitespaceStart . trimWhitespaceEnd
