a37c0c4f9b72c6f7fcf2852f0b3ad5fa28d35c16
Paul Suda <paulsuda@gmail.com>
2016-12-24 17:39:59 -0600

    Running time measurement per challenge.

--- Main.hs
+++ Main.hs
@@ -6,6 +6,7 @@ import qualified Challenge3.RunChallenge
 import qualified Challenge4.RunChallenge
 import qualified Challenge5.RunChallenge
 import qualified Challenge6.RunChallenge
+import Data.Time.Clock (getCurrentTime, diffUTCTime)
 
 import Text.PrettyPrint.ANSI.Leijen
 
@@ -14,15 +15,20 @@ main = do
   runChallenge("Challenge 1", Challenge1.RunChallenge.run)
   runChallenge("Challenge 2", Challenge2.RunChallenge.run)
   runChallenge("Challenge 3", Challenge3.RunChallenge.run)
-  -- runChallenge("Challenge 4", Challenge4.RunChallenge.run)
+  runChallenge("Challenge 4", Challenge4.RunChallenge.run)
   runChallenge("Challenge 5", Challenge5.RunChallenge.run)
   runChallenge("Challenge 6", Challenge6.RunChallenge.run)
 
+runChallenge String -> () -> IO()
 runChallenge (label, fn) = do
   let extendedLine = replicate (60 - length label) '-'
   let labelText = "------- [ " ++ label ++ "] ---" ++ extendedLine
   putDoc $ dullyellow (text labelText) <> linebreak
-  fn(greenText, redText, blueText)
+  start <- getCurrentTime
+  retVal <- fn(greenText, redText, blueText)
+  end <- getCurrentTime
+  let runSeconds = diffUTCTime end start
+  putDoc $ dullyellow (text ("Run time " ++ show runSeconds ++ "sec.")) <> linebreak
 
 blueText :: String -> IO()
 blueText message = putDoc $ blue (text message) <> linebreak
