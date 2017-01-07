f8c10a687ad06adcf8db5286453bf4ce5f3a6ec7
Paul Suda <paulsuda@gmail.com>
2017-01-06 19:47:27 -0600

    Better clearer base64 code

--- shared/Base64.hs
+++ shared/Base64.hs
@@ -11,23 +11,37 @@ decodeChar chr = if null indexesFound then 0 else head indexesFound
 encodeChar :: Int -> Char
 encodeChar bits = base64Codes !! (bits `mod` 64)
 
+
+
 encode :: String -> String
 encode "" = ""
-encode (a:b:c:x) = [ encodeChar(bytes `quot` (64 ^ 3)),  encodeChar(bytes `quot` (64 ^ 2)), encodeChar(bytes `quot` 64), encodeChar bytes ] ++ encode x
-  where bytes = fromEnum c + (fromEnum b * (2 ^ 8)) + (fromEnum a * (2 ^ 16))
-encode (a:b:x) = [ encodeChar(bytes `quot` (64 ^ 3)),  encodeChar(bytes `quot` (64 ^ 2)), encodeChar(bytes `quot` 64), '=' ]
-  where bytes = (fromEnum b * (2 ^ 8)) + (fromEnum a * (2 ^ 16))
-encode (a:x) = [ encodeChar(bytes `quot` (64 ^ 3)),  encodeChar(bytes `quot` (64 ^ 2)), '=', '=' ]
-  where bytes = (fromEnum a * (2 ^ 16))
+encode (a:b:c:x) = [ encodedCharFromInt bytes 3,  encodedCharFromInt bytes 2, encodedCharFromInt bytes 1, encodedCharFromInt bytes 0 ] ++ encode x
+  where bytes = intFromBytes a b c
+encode (a:b:x) = [ encodedCharFromInt bytes 3,  encodedCharFromInt bytes 2, encodedCharFromInt bytes 1, '=' ]
+  where bytes = intFromBytes a b '\0'
+encode (a:x) = [ encodedCharFromInt bytes 3,  encodedCharFromInt bytes 2, '=', '=' ]
+  where bytes = intFromBytes a '\0' '\0'
 
-
--- # TODO: "=" at end of short
 decode :: String -> String
 decode "" = ""
-decode (a:b:"==") = [ toEnum(bytes `div` (2 ^ 16) `mod` 256) ]
-  where bytes = decodeChar a * (64 ^ 3) + decodeChar b * (64 ^ 2)
-decode (a:b:c:"=") = [ toEnum(bytes `div` (2 ^ 16) `mod` 256), toEnum(bytes `div` (2 ^ 8) `mod` 256) ]
-  where bytes = decodeChar a * (64 ^ 3) + decodeChar b * (64 ^ 2) + decodeChar c * 64
-decode (a:b:c:d:x) = [ toEnum(bytes `div` (2 ^ 16) `mod` 256), toEnum(bytes `div` (2 ^ 8) `mod` 256), toEnum(bytes `mod` 256) ] ++ nextStr
-  where bytes = decodeChar a * (64 ^ 3) + decodeChar b * (64 ^ 2) + decodeChar c * 64 + decodeChar d
+decode (a:b:"==") = [ byteFromInt bytes 16 ]
+  where bytes = intFromEncodedChars a b '\0' '\0'
+decode (a:b:c:"=") = [ byteFromInt bytes 16, byteFromInt bytes 8 ]
+  where bytes = intFromEncodedChars a b c '\0'
+decode (a:b:c:d:x) = [ byteFromInt bytes 16, byteFromInt bytes 8, byteFromInt bytes 0 ] ++ nextStr
+  where bytes = intFromEncodedChars a b c d
         nextStr = decode x
+
+-- - - Private - -
+
+byteFromInt :: Int -> Int -> Char
+byteFromInt bytes bitIndex = toEnum(bytes `div` (2 ^ bitIndex) `mod` 256)
+
+intFromEncodedChars :: Char -> Char -> Char -> Char -> Int
+intFromEncodedChars a b c d = decodeChar a * (64 ^ 3) + decodeChar b * (64 ^ 2) + decodeChar c * 64 + decodeChar d
+
+intFromBytes :: Char -> Char -> Char -> Int
+intFromBytes a b c = (fromEnum a * (2 ^ 16)) + (fromEnum b * (2 ^ 8)) + fromEnum c
+
+encodedCharFromInt :: Int -> Int -> Char
+encodedCharFromInt bytes charIndex = encodeChar(bytes `quot` (64 ^ charIndex))
