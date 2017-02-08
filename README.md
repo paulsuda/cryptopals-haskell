fa5044d83789b1ff5dd8ce97440b8cfa379226bc
Paul Suda <paulsuda@gmail.com>
2017-02-07 19:45:21 -0600

    Getting started on 7, notes, try to make cryptonite work

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 Doing the (cryptopals.com)[https://cryptopals.com/] crypto challenges in Haskell, and trying to learn Haskell at the same time.
 
-* A lot of stuff here replicates features available in Haskell modules. I preferred to write my own string process and other functions to get practice coding Haskell. There are much more sensible ways to do this stuff.
+## Notes:
+
+### Set 1 Parts 1 through 6:
+
+A lot of stuff here replicates features available in Haskell modules. I preferred to write my own string process and other functions to get practice coding Haskell. There are much more sensible ways to do this stuff.
+
+### Set 1 Parts 7:
+
+OK, not going to write AES from scratch and I'm seeing how slow native Haskell strings can be. Time to
+get a crypto library and start using Data.String.
---
+++ challenge7/RunChallenge.hs
@@ -0,0 +1,27 @@
+module Challenge7.RunChallenge (main, run) where
+
+-- https://hackage.haskell.org/package/cryptonite-0.21/docs/Crypto-Tutorial.html
+--
+-- import Data.ByteString (ByteString)
+--
+-- import Crypto.Cipher.AES (AES128)
+-- import Crypto.Cipher.Types (BlockCipher(..), Cipher(..),nullIV)
+-- import Crypto.Error (CryptoFailable(..))
+
+-- secretKey :: ByteString
+-- secretKey = "012-456-89A-CDE-012-456-89A-CDE-"
+
+--
+-- secretKey :: ByteString
+-- secretKey = "YELLOW SUBMARINE" :: ByteString
+
+main :: IO ()
+main = run(putStrLn, putStrLn, putStrLn)
+
+
+run (putResult, putError, putStatus) = do
+
+  cipherTextFile <- readFile "challenge7/ciphertext.txt"
+  -- let plainText = trimWhitespace plainTextFile
+  if False then putResult "OK! Expected result."
+  else putError "ERROR! Result not as expected."
