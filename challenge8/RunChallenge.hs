6f80dac415b3001292ea370a32c5d6ec8e8241c1
Paul Suda <paulsuda@gmail.com>
2017-05-22 22:07:19 -0500

    Challenge 8 done, I think

--- challenge8/RunChallenge.hs
+++ challenge8/RunChallenge.hs
@@ -3,23 +3,48 @@ module Challenge8.RunChallenge (main, run) where
 
 import Shared.Challenge (ChallengeRunner)
 
+import Shared.Histogram (HistValue, HistArray, histMin, histMax)
+import Shared.Views (histShow)
+
 -- import Shared.Hex as Hex
 import Data.Hex as Hex
 import Data.ByteString.Char8 as ByteString
+import Data.List (nub)
+import Data.List.Split (chunksOf)
 
 main :: IO ()
 main = run Prelude.putStrLn Prelude.putStrLn Prelude.putStrLn
 
 cipherTextLines :: ByteString -> [ByteString]
-cipherTextLines s = do
-  return (Prelude.map Hex.unhex (ByteString.lines s))
+cipherTextLines s = Prelude.head dataLines
+  where lines = ByteString.lines s
+        dataLines = Prelude.mapM Hex.unhex lines
+
+chunkData :: ByteString -> [ByteString]
+chunkData xs = ByteString.take blockSize xs : remaining
+  where blockSize = 16
+        remaining = if ByteString.length xs > blockSize
+                      then chunkData (ByteString.drop blockSize xs)
+                      else []
+
+mostRepeatCount :: ByteString -> Int
+mostRepeatCount s = chunksLength - uniqueChunksLength
+  where uniqueChunksLength = Prelude.length uniqueChunks
+        uniqueChunks = nub chunks
+        chunksLength = Prelude.length chunks
+        chunks = chunkData s
+
+mostRepeatsHist :: [ByteString] -> HistArray
+mostRepeatsHist [] = []
+mostRepeatsHist dataLines = fromIntegral (mostRepeatCount (Prelude.head dataLines)) : mostRepeatsHist (Prelude.tail dataLines)
 
 run :: ChallengeRunner
 run putResult putError putStatus = do
   cipherTextFile <- ByteString.readFile "challenge8/ciphertext.txt"
-  -- let cipherTextLines = ByteString.lines cipherTextFile
   let cipherTextData = cipherTextLines cipherTextFile
-  -- let cipherTextData = Hex.unhex cipherTextLines
-
-  if False then putResult "OK! Expected result."
+  let scoreHist = mostRepeatsHist cipherTextData
+  let (maxIndex, maxScore) = histMax scoreHist
+  putStatus(histShow scoreHist ++ "Found item index " ++ show maxIndex ++ " with score " ++ show maxScore)
+  -- Maybe 132 is correct, not sure??
+  if maxIndex == 132 then putResult "OK! Expected result."
   else putError "ERROR! Result not as expected."
